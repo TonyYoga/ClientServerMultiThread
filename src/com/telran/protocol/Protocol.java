36dd0e1bcc085bc205ab6be892af6ed7ead6cb0f
Anton Konkin <konkin.anton@gmail.com>
2019-05-09 23:42:08 +0300

    RawHttpResponse, Protoclo added

---
+++ src/com/telran/protocol/Protocol.java
@@ -0,0 +1,6 @@
+package com.telran.protocol;
+
+public interface Protocol {
+    RawHttpResponse getResponse(RawHttpRequest request);
+    RawHttpResponse gerErrorResponse(int code, String reasonPhraseig, String errorBody)
+}
--- src/com/telran/protocol/RawHttpResponse.java
+++ src/com/telran/protocol/RawHttpResponse.java
@@ -1,4 +1,40 @@
 package com.telran.protocol;
 
+import java.util.Map;
+
 public class RawHttpResponse {
+    public static class ResponseCode {
+        public static final int OK = 200;
+        public static final int BAD_REQUEST = 300;
+        public static final int SERVER_ERROR = 500;
+    }
+
+    public int code;
+    public String reasonPhrase;
+    public Map<String, String> headers;
+    public String body;
+
+    public RawHttpResponse(int code, String reasonPhrase, Map<String, String> headers, String body) {
+        this.code = code;
+        this.reasonPhrase = reasonPhrase;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("HTTP/1.1 " + code);
+        if (reasonPhrase != null) {
+            builder.append(" " + reasonPhrase);
+            for (Map.Entry<String, String> header : headers.entrySet()) {
+                builder.append(header.getKey() + ":" + header.getValue() + "\r\n");
+            }
+            builder.append("\r\n");
+            if (body != null) {
+                builder.append(body);
+            }
+        }
+        return builder.toString();
+    }
 }
