a43c5b9072488d468c0ca98990d86f0458ec3f86
Anton Konkin <konkin.anton@gmail.com>
2019-05-14 17:15:05 +0300

    RawHttpResponse.java

--- src/com/telran/protocol/RawHttpResponse.java
+++ src/com/telran/protocol/RawHttpResponse.java
@@ -6,6 +6,7 @@ public class RawHttpResponse {
     public static class ResponseCode {
         public static final int OK = 200;
         public static final int BAD_REQUEST = 300;
+        public static final int NOT_FOUND = 404;
         public static final int SERVER_ERROR = 500;
     }
 
